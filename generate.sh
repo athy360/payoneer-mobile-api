66b04f3d7fd2715b487781ad41696a96a8d6dc22
Alexey Pelykh <alexey.pelykh@gmail.com>
2017-04-11 08:31:08 +0300

    Generation

---
+++ generate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CODEGEN_VERSION="2.2.2"
+
+mkdir -p bin
+if [ ! -f "bin/swagger-codegen-cli.jar" ]; then
+    curl -o "bin/swagger-codegen-cli.jar" \
+        "https://oss.sonatype.org/content/repositories/releases/io/swagger/swagger-codegen-cli/$CODEGEN_VERSION/swagger-codegen-cli-$CODEGEN_VERSION.jar"
+fi
+
+ACTUAL_CODEGEN_VERSION=$(java -jar "bin/swagger-codegen-cli.jar" version)
+if [ "$ACTUAL_CODEGEN_VERSION" != "$CODEGEN_VERSION" ]; then
+    echo "Expected swagger-codegen-cli version $CODEGEN_VERSION, actual version $ACTUAL_CODEGEN_VERSION"
+    exit 1
+fi
+
+CODEGEN="java -jar bin/swagger-codegen-cli.jar"
+CONFIG=".codegen.json"
+TAG=$(git describe --tags)
+VERSION="${TAG/v/}"
+
+echo "{\"packageName\":\"payoneer_mobile_api\",\"packageVersion\":\"$VERSION\"}" > $CONFIG
+$CODEGEN generate -i swagger.yaml -l python -o python -c $CONFIG
